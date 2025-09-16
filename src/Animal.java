f9a9df725cd5f4d21bc31ad934d1b5a9c1ccb9ea
Bilel <avebilel10@gmail.com>
2025-09-16 13:08:50 +0200

    Add Prosit 2 code (Zoo and Animal classes)

---
+++ src/Animal.java
@@ -0,0 +1,25 @@
+public class Animal {
+    String family;
+    String name;
+    int age;
+    boolean isMammal;
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
+    public void display() {
+        System.out.println("Animal: " + name +
+                " | Famille: " + family +
+                " | Âge: " + age +
+                " | Mammifère: " + isMammal);
+    }
+    @Override
+    public String toString() {
+        return "Animal{name='" + name +
+                "', family='" + family +
+                "', age=" + age +
+                ", isMammal=" + isMammal + "}";
+    }
+}
